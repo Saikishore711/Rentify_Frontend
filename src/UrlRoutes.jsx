f15ce947539a3b3a23bf7a0e455b7e180f01271f
Vishal <vishalvk1256@gmail.com>
2024-05-26 16:02:57 +0530

    Initial Setup

--- src/UrlRoutes.jsx
+++ src/UrlRoutes.jsx
@@ -1,11 +1,25 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Hello from './screens/Hello';
+import Login from "./screens/Login";
+import BuyerHome from "./screens/Buyer/BuyerHome";
+import SellerHome from "./screens/Seller/SellerHome";
+import Register from "./screens/Register";
 
 export default function Url_Routes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/buyer">
+          <Route index element={<BuyerHome />} />
+        </Route>
+
+        <Route path="/seller">
+          <Route index element={<SellerHome />} />
+        </Route>
+        
         <Route path="*" element={<Navigate to="/" replace={true} />} />
       </Routes>
     </BrowserRouter>
